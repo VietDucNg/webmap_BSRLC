250143a194b9ffeb3cdc80a54e36c322dd3fbb96
VietDucNg <vietducnguyen.fit@gmail.com>
2026-06-25 12:55:56 +0200

    build: Logo component

---
+++ src/components/Header/Logo.jsx
@@ -0,0 +1,15 @@
+import { Box, Stack, Typography } from "@mui/material";
+
+export default function Logo() {
+  return (
+    <Stack direction={"row"} sx={{ gap: 1, color: "white" }}>
+      <Typography sx={{ fontSize: "30px", mx: 1 }}>🍀</Typography>
+      <Box>
+        <Typography variant="h5" component={"h1"}>
+          Baltic Sea Region Land Cover
+        </Typography>
+        <Typography>Annual dataset 2000 - 2022</Typography>
+      </Box>
+    </Stack>
+  );
+}
