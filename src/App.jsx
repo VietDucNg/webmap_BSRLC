2a81be8bfd84c3a25a96e89d5dba19d2544a0758
VietDucNg <vietducnguyen.fit@gmail.com>
2026-06-25 14:20:08 +0200

    build!: skeleton of leftDrawer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { ThemeProvider } from "@mui/material/styles";
-import { CssBaseline, Container } from "@mui/material";
+import { CssBaseline, Container, Stack } from "@mui/material";
 import getTheme from "./theme";
 import { useEffect, useMemo, useState } from "react";
 import Header from "./components/Header/Header";
+import LeftDrawer from "./components/LeftDrawer/LeftDrawer";
 
 function App() {
   // handle color mode
@@ -20,11 +21,31 @@ function App() {
     [mode],
   );
 
+  // left drawer
+  const [isLeftDrawerOpen, setIsLeftDrawerOpen] = useState(false);
+  const handleDrawerOpen = () => {
+    setIsLeftDrawerOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setIsLeftDrawerOpen(false);
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container maxWidth="none" sx={{ my: 1, maxWidth: "1800px" }}>
-        <Header mode={mode} setMode={setMode} />
+        <Header
+          mode={mode}
+          setMode={setMode}
+          open={isLeftDrawerOpen}
+          handleDrawerOpen={handleDrawerOpen}
+        />
+        <Stack>
+          <LeftDrawer
+            open={isLeftDrawerOpen}
+            handleDrawerClose={handleDrawerClose}
+          />
+        </Stack>
       </Container>
     </ThemeProvider>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,83 @@
-import { Box, IconButton, Stack } from "@mui/material";
+import { Box, IconButton, Stack, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { styled } from "@mui/material/styles";
+import MuiAppBar from "@mui/material/AppBar";
+
 import Logo from "./Logo";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
 
-export default function Header({ mode, setMode }) {
-  function changeMode() {
-    setMode((prev) => (prev === "dark" ? "light" : "dark"));
-  }
+function changeMode() {
+  setMode((prev) => (prev === "dark" ? "light" : "dark"));
+}
 
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(["width", "margin"], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
+    },
+  ],
+}));
+
+export default function Header({ mode, setMode, open, handleDrawerOpen }) {
   return (
-    <Stack
-      direction={"row"}
-      sx={{
-        flexWrap: "wrap",
-        gap: 1,
-        justifyContent: "space-between",
-        backgroundColor: "header.main",
-        padding: 1,
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10,
-      }}
-    >
-      <Logo />
-      <Box>
-        <IconButton onClick={changeMode} sx={{ color: "header.contrastText" }}>
-          {mode === "dark" ? <DarkModeIcon /> : <LightModeIcon />}
+    <AppBar position="fixed" open={open}>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          sx={[
+            {
+              marginRight: 5,
+            },
+            open && { display: "none" },
+          ]}
+        >
+          <MenuIcon />
         </IconButton>
-      </Box>
-    </Stack>
+        <Typography variant="h6" noWrap component="div">
+          Mini variant drawer
+        </Typography>
+      </Toolbar>
+    </AppBar>
+
+    // <Stack
+    //   direction={"row"}
+    //   sx={{
+    //     flexWrap: "wrap",
+    //     gap: 1,
+    //     justifyContent: "space-between",
+    //     backgroundColor: "header.main",
+    //     padding: 1,
+    //     borderTopLeftRadius: 10,
+    //     borderTopRightRadius: 10,
+    //   }}
+    // >
+    //   <Logo />
+    //   <Box>
+    //     <IconButton onClick={changeMode} sx={{ color: "header.contrastText" }}>
+    //       {mode === "dark" ? <DarkModeIcon /> : <LightModeIcon />}
+    //     </IconButton>
+    //   </Box>
+    // </Stack>
   );
 }
